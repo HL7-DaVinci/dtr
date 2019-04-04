5807d76fcbe87db0c22989462e6c026f501e45ea
Cole Springate-Combs <cspringatecombs@mitre.org>
2019-04-04 15:24:54 -0400

    rebase and fix

--- src/components/Inputs/OpenChoiceInput/OpenChoice.jsx
+++ src/components/Inputs/OpenChoiceInput/OpenChoice.jsx
@@ -8,7 +8,7 @@ export default class OpenChoice extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: {display:""},
+            values: [],
             open: false,
             choices:[],
             display:""
@@ -16,75 +16,156 @@ export default class OpenChoice extends Component {
 
     this.onInputChange = this.onInputChange.bind(this);
     this.setChoice = this.setChoice.bind(this);
+    this.ref = React.createRef();
+
     }
 
     componentWillMount() {
         const returnAnswer = getListOfChoices(this.props, this.setChoice);
         if(returnAnswer) {
-            this.setState({"value":returnAnswer});
+            this.setState({"values":[returnAnswer]});
+            if(!this.props.item.repeats) {
+                this.setState({"display":returnAnswer.display});
+            }
             this.props.updateCallback(this.props.item.linkId, returnAnswer,"values");
         }
         
     }
 
     componentDidMount() {
-        this.props.updateCallback(this.props.item.linkId,  {"type":this.props.inputTypeDisplay,"text":this.props.item.text,"valueType":"valueCoding"}, "itemTypes")
+        this.props.updateCallback(this.props.item.linkId,  
+            {"type":this.props.inputTypeDisplay,
+            "text":this.props.item.text,
+            "valueType":"valueCoding",
+            "ref":this.ref}, "itemTypes")
     }
     onInputChange(event) {
-        console.log(this.state.value);
-        console.log(event.target.value);
-        this.setState({value: event.target.value})
+        this.setState({display: event.target.value})
+        if(!this.props.item.repeats) {
+            this.setState({"values":[{display:event.target.value}]})
+        }
         this.props.updateCallback(this.props.item.linkId, event.target.value,"values")
-        if (this.state.choices.findIndex(p => p.code == event.target.value) === -1) {
-            this.props.updateCallback(this.props.item.linkId,  {"type":this.props.inputTypeDisplay,"text":this.props.item.text,"valueType":"valueString"}, "itemTypes")
+        if (this.state.choices.findIndex(p => p.code == event.target.value) === -1 && !this.props.item.repeats) {
+            this.props.updateCallback(this.props.item.linkId,  
+                {"type":this.props.inputTypeDisplay,
+                "text":this.props.item.text,
+                "valueType":"valueString",
+                "ref":this.ref}, "itemTypes")
         }else{
-            this.props.updateCallback(this.props.item.linkId,  {"type":this.props.inputTypeDisplay,"text":this.props.item.text,"valueType":"valueCoding"}, "itemTypes")
+            this.props.updateCallback(this.props.item.linkId,  
+                {"type":this.props.inputTypeDisplay,
+                "text":this.props.item.text,
+                "valueType":"valueCoding",
+                "ref":this.ref}, "itemTypes")
         }
     }
 
     checkStateForValue(value) {
         return ;
     }
+
     setChoice(pair) {
         this.setState(previousState => ({
             choices: [...previousState.choices, pair]
         }));
     }
 
+    addOption(e) {
+        const newArray = [...this.state.values, e];
+        this.setState({values:newArray});
+        this.props.updateCallback(this.props.item.linkId, newArray,"values");
+
+        return newArray;
+    }
+
+    saveToDisplay(e){
+        if(this.props.item.repeats){
+            if(this.state.display.trim().length>0) {
+                if(this.state.values.filter((el)=>{return el.display.trim()===this.state.display.trim()}).length===0){
+                    this.addOption({display:this.state.display, valueTypeFinal:"valueString"});
+                }
+            }
+            this.setState({display:""});
+        }else{
+            e.target.blur();
+        }
+    }
     render() {
         return (
-            <div className="open-choice">
+            <div className="open-choice" ref={this.ref}>
                 <div className="text-input-label">{this.props.item.type}</div>
                 <div className="dropdown">
-                    <div className="dropdown-input"
+                    <div className={"dropdown-input " + (this.props.item.repeats?"repeated-choice":"")}
                     tabIndex="0"
-                    onBlur={()=>{
+                    onBlur={(e)=>{
                         this.setState({open:false})
+                        this.saveToDisplay(e);
                     }}
-                    >
-                        <input value = {this.state.value.display?this.state.value.display:this.state.value} 
-                        className={"input-block top-block " + (this.props.item.repeats?"repeated-choice":null)}
-                        onClick={()=>{
+                    onClick={()=>{
+                        if(this.props.item.repeats) {
+                            this.myInp.focus();
+                            this.setState(prevState=>({
+                                open:true
+                            }))
+                        }else{
                             this.setState(prevState=>({
                                 open:!prevState.open
                             }))
-                        }} 
+                        }
+
+                    }} 
+                    >
+                    {this.props.item.repeats?this.state.values.map((value)=>{
+                        return <a 
+                                    key={value.display} 
+                                    className="selected-value"
+                                    onClick={()=>{
+                                        const newArray = this.state.values.filter((e)=>{
+                                            return e.display!==value.display;
+                                        })
+                                        this.setState({values:newArray});
+                                        this.props.updateCallback(this.props.item.linkId, newArray,"values");
+                                    }}
+                                    onMouseDown={(event)=>{event.preventDefault()}}
+                                >
+                                    {value.display}
+                                </a>
+                    }):null}
+
+                        <input 
+                        ref={(ip)=>this.myInp = ip}
+                        value = {this.state.display} 
+                        style={{width:this.state.display.length+2 + "ch"}}
+                        className={"input-block top-block " + (this.props.item.repeats?"repeated-input":"")}
                         spellCheck="false"
                         onChange={this.onInputChange}
                         onKeyPress={(e)=>{
                             if(e.key==="Enter"){
-                                e.target.blur();
+                                this.saveToDisplay(e);
                             }
-                        }}/>
-                        <div className={"dropdown-block option-block " + (this.state.open?'':"hide-block")}>
+                        }}
+                            />
+
+                        <div className={"dropdown-block option-block " + (this.state.open?'':"hide-block ") + (this.props.item.repeats?"repeated-choice":"")}>
                             {this.state.choices.map((e)=>{
-                                if(e.display!=this.state.value.display){
+                                if(this.state.values.filter((el)=>{return el.display===e.display}).length===0){
                                     return (
                                         <div key={e.code} className="unselected-option" onClick={()=>{
-                                            this.setState({value:e});
-                                            this.setState({open:false});
-                                            this.props.updateCallback(this.props.item.linkId, e,"values");
-                                            this.props.updateCallback(this.props.item.linkId,  {"type":this.props.inputTypeDisplay,"text":this.props.item.text,"valueType":"valueCoding"}, "itemTypes")
+                                            if(this.props.item.repeats) {
+                                                const newArray = this.addOption(e);
+                                                this.props.updateCallback(this.props.item.linkId, newArray,"values");
+                                            }else{
+                                                
+                                                this.setState({"values":[e]});
+                                                this.setState({"display":e.display});
+                                                this.props.updateCallback(this.props.item.linkId, [e],"values");
+                                                this.props.updateCallback(this.props.item.linkId,  
+                                                    {"type":this.props.inputTypeDisplay,
+                                                    "text":this.props.item.text,
+                                                    "valueType":"valueCoding",
+                                                    "ref":this.ref}, "itemTypes");
+                                            }
+                                            
                                         }}
                                         // prevent the dropdown from stealing focus and closing
                                         onMouseDown={(event)=>{event.preventDefault()}}>
