c73d87a4aa8cb342ad32ae7eaf3bc17ed2c799f7
Keeyan Ghoreshi <kghoreshi@mitre.org>
2019-04-29 17:28:04 -0400

    changes to recieve app context

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import App from "./App.js";
 // get the URL parameters received from the authorization server
 const state = urlUtils.getUrlParameter("state"); // session key
 const code = urlUtils.getUrlParameter("code"); // authorization code
-console.log("state: " + state);
-console.log("code: "+code);
-console.log(window.location);
+
 // load the app parameters stored in the session
 const params = JSON.parse(sessionStorage[state]); // load app session
 const tokenUri = params.tokenUri;
@@ -24,28 +22,59 @@ const redirectUri = params.redirectUri;
 // the proxy set up in webpack.config.dev.js so the CRD server needs to be running
 const FHIR_URI_PREFIX = "../fetchFhirUri/";
 
+const data = new URLSearchParams();
+data.append("code", code);
+data.append("grant_type", "authorization_code");
+data.append("redirect_uri", redirectUri);
+if (!secret) data.append("client_id", clientId);
+
+const headers = {
+  "Content-Type": "application/x-www-form-urlencoded"
+};
+if (secret) headers["Authorization"] = "Basic " + btoa(clientId + ":" + secret);
+
+// obtain authorization token from the authorization service using the authorization code
+const pat = fetch(tokenUri, {
+  method: "POST",
+  headers: headers,
+  body: data.toString()
+})
+  .then(res => res.json())
+  // should get back the access token and maybe the patient ID
+  .then(function (auth_response) {
+    const appContext = {
+        template: auth_response.template,
+        request: serviceUri + '/' + auth_response.request
+      }
+      // hardcoded smart, should be set up with context stuff
+      var smart = FHIR.client({
+        serviceUrl: serviceUri,
+        patientId: auth_response.patient
+      });
+      
+      
+      ReactDOM.render(
+        <App
+          FHIR_URI_PREFIX={FHIR_URI_PREFIX}
+          questionnaireUri={appContext.template}
+          smart={smart}
+          deviceRequestUri={appContext.request}
+        />,
+        document.getElementById("root")
+      );
+      console.log(auth_response);
+    const patientId = auth_response.patient;
+    if (patientId == null) {
+      const errorMsg = "Failed to get a patientId from the app params or the authorization response.";
+      document.body.innerText = errorMsg;
+      console.error(errorMsg);
+      return;
+    }
+    return patientId;
+  });
 
 // hardcoded appContext, needs to by retrieved from OAuth 2.0 access token response
-const appContext = {
-  template: "urn:hl7:davinci:crd:home-oxygen-questionnaire",
-  request: "http://localhost:8080/ehr-server/stu3/DeviceRequest/devreq013/"
-}
-// hardcoded smart, should be set up with context stuff
-var smart = FHIR.client({
-  serviceUrl: "http://localhost:8080/ehr-server/stu3",
-  patientId: "pat013"
-});
 
-
-ReactDOM.render(
-  <App
-    FHIR_URI_PREFIX={FHIR_URI_PREFIX}
-    questionnaireUri={appContext.template}
-    smart={smart}
-    deviceRequestUri={appContext.request}
-  />,
-  document.getElementById("root")
-);
 
 // const valueSetDB = {};
 
