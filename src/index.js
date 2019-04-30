fba0e2ccd448c99cbcaceca7559de9e876e0773e
vnprathima <prathima@affosoft.com>
2019-04-30 18:51:41 +0530

    Oauth 2.0 Authentication added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import urlUtils from "./util/url";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-import { createToken } from './components/Authentication';
+// import { createToken } from './components/Authentication';
 
 
 
@@ -13,8 +13,8 @@ import { createToken } from './components/Authentication';
 // the proxy set up in webpack.config.dev.js so the CRD server needs to be running
 const FHIR_URI_PREFIX = "../fetchFhirUri/";
 
-async function getMessageDef(token) {
-  var tempURL = "http://3.92.187.150:8280/fhir/baseDstu3/MessageDefinition/15105";
+async function getMessageDef(token, launchContextId) {
+  var tempURL = "http://3.92.187.150:8280/fhir/baseDstu3/MessageDefinition/"+launchContextId;
   let req = fetch(tempURL, {
     method: 'GET',
     headers: {
@@ -38,15 +38,60 @@ export const appContext = {
   npi: ""
 }
 
+async function getToken() {
+  return new Promise(resolve => {
+    // get the URL parameters received from the authorization server
+    const state = urlUtils.getUrlParameter("state"); // session key
+    const code = urlUtils.getUrlParameter("code"); // authorization code
+
+    // load the app parameters stored in the session
+    const params = JSON.parse(sessionStorage[state]); // load app session
+    console.log("Params----",params);
+    const tokenUri = params.tokenUri;
+    const clientId = params.clientId;
+    const secret = params.secret;
+    const redirectUri = params.redirectUri;
+
+    // Prep the token exchange call parameters
+    const data = new URLSearchParams();
+    data.append("code", code);
+    data.append("grant_type", "authorization_code");
+    data.append("redirect_uri", redirectUri);
+    if (!secret) data.append("client_id", clientId);
+
+    const headers = {
+      "Content-Type": "application/x-www-form-urlencoded"
+    };
+    if (secret) headers["Authorization"] = "Basic " + btoa(clientId + ":" + secret);
+
+    // obtain authorization token from the authorization service using the authorization code
+    fetch(tokenUri, {
+      method: "POST",
+      headers: headers,
+      body: data.toString()
+    }).then(res => res.json())
+      // should get back the access token and maybe the patient ID
+      .then(function (auth_response) {
+        resolve(auth_response.access_token);
+      });
+  });
+}
 
 // hardcoded smart, should be set up with context stuff
 async function getFromFhir() {
-  let token = await createToken('john', 'john123');
-  let msg_def = await getMessageDef(token);
+  let token = await getToken();
+   const state = urlUtils.getUrlParameter("state"); // session key
+  // load the app parameters stored in the session
+  const params = JSON.parse(sessionStorage[state]); // load app session
+  const serviceUri = params.serviceUri;
+  const launchContextId = params.launchContextId;
+
+  let msg_def = await getMessageDef(token,launchContextId);
+ 
   var smart;
-  // console.log("message def----", msg_def);
   if (msg_def.description != undefined) {
-    let resp = JSON.parse('{' + decodeURIComponent(msg_def.description) + '}');
+    console.log("messagw def---",decodeURIComponent(msg_def.description));
+    let resp = JSON.parse(decodeURIComponent(msg_def.description));
     let app_context = resp.appContext;
     if (app_context.file_links) {
       if (app_context.file_links.questionnaire_file != undefined) {
@@ -55,23 +100,30 @@ async function getFromFhir() {
       }
     }
     if (app_context.appData) {
+      if (app_context.appData.Practitioner != undefined) {
+        appContext.npi = app_context.appData.Practitioner;
+      }
       if (app_context.appData.patientId != undefined) {
         appContext.patientId = app_context.appData.patientId;
-        smart = FHIR.client({
-          serviceUrl: "http://3.92.187.150:8280/fhir/baseDstu3",
+
+        // const patientId = patientIdfromAppParams || auth_response.patient;
+        if (appContext.patientId == null) {
+          errorMsg = "Failed to get a patientId from the app params or the authorization response.";
+          document.body.innerText = errorMsg;
+          console.error(errorMsg);
+          return;
+        }
+        var smart = FHIR.client({
+          serviceUrl: serviceUri,
           patientId: appContext.patientId,
           auth: {
-            type: 'bearer',
+            type: "bearer",
             token: token
           }
         });
       }
-      if (app_context.appData.Practitioner != undefined) {
-        appContext.npi = app_context.appData.Practitioner;
-      }
     }
   }
-  console.log("app context", appContext);
   return smart;
 }
 
@@ -87,8 +139,6 @@ getFromFhir().then((smart) => {
     document.getElementById("root")
   );
 });
-
-
 
 // const valueSetDB = {};
 
--- src/launch.js
+++ src/launch.js
@@ -11,98 +11,107 @@ var secret = null; // set me, if confidential
 
 // These parameters will be received at launch time in the URL
 var serviceUri = urlUtils.getUrlParameter("iss");
-var launchContextId = urlUtils.getUrlParameter("launch");
-var patientId = urlUtils.getUrlParameter("patientId");
 
-// The scopes that the app will request from the authorization server
-// encoded in a space-separated string:
-//      1. permission to read all of the patient's record
-//      2. permission to launch the app in the specific context
-var scope = ["patient/*.read", "launch"].join(" ");
+if (serviceUri) {
+  var launchContextId = urlUtils.getUrlParameter("launch");
+  var patientId = urlUtils.getUrlParameter("patientId");
 
-// Generate a unique session key string (here we just generate a random number
-// for simplicity, but this is not 100% collision-proof)
-var state = Math.round(Math.random() * 100000000).toString();
+  // The scopes that the app will request from the authorization server
+  // encoded in a space-separated string:
+  //      1. permission to read all of the patient's record
+  //      2. permission to launch the app in the specific context
+  var scope = ["patient/*.read", "launch"].join(" ");
 
-// To keep things flexible, let's construct the launch URL by taking the base of the
-// current URL and replace "launch.html" with "index.html".
-var launchUri = window.location.protocol + "//" + window.location.host + window.location.pathname;
-var redirectUri = launchUri.replace("launch", "index");
+  // Generate a unique session key string (here we just generate a random number
+  // for simplicity, but this is not 100% collision-proof)
+  var state = Math.round(Math.random() * 100000000).toString();
 
-// FHIR Service Conformance Statement URL
-var conformanceUri = serviceUri + "/metadata";
+  // To keep things flexible, let's construct the launch URL by taking the base of the
+  // current URL and replace "launch.html" with "index.html".
+  var launchUri = window.location.protocol + "//" + window.location.host + window.location.pathname;
+  var redirectUri = launchUri.replace("launch", "index");
 
-// Let's request the conformance statement from the SMART on FHIR API server and
-// find out the endpoint URLs for the authorization server
-const conformanceGet = new XMLHttpRequest();
-conformanceGet.open("GET", conformanceUri);
-conformanceGet.onload = function() {
-  if (conformanceGet.status === 200) {
-    let conformanceStatement;
-    try {
-      conformanceStatement = JSON.parse(conformanceGet.responseText);
-    } catch (e) {
-      errorMsg = "Unable to parse conformance statement.";
+  // FHIR Service Conformance Statement URL
+  var conformanceUri = serviceUri + "/metadata";
+
+  // Let's request the conformance statement from the SMART on FHIR API server and
+  // find out the endpoint URLs for the authorization server
+  const conformanceGet = new XMLHttpRequest();
+  conformanceGet.open("GET", conformanceUri);
+  conformanceGet.onload = function () {
+    if (conformanceGet.status === 200) {
+      let conformanceStatement;
+      try {
+        conformanceStatement = JSON.parse(conformanceGet.responseText);
+      } catch (e) {
+        errorMsg = "Unable to parse conformance statement.";
+        document.body.innerText = errorMsg;
+        console.error(errorMsg);
+        return;
+      }
+      redirect(conformanceStatement);
+    } else {
+      errorMsg = "Conformance statement request failed. Returned status: " + conformanceGet.status;
       document.body.innerText = errorMsg;
       console.error(errorMsg);
       return;
     }
-    redirect(conformanceStatement);
-  } else {
-    errorMsg = "Conformance statement request failed. Returned status: " + conformanceGet.status;
-    document.body.innerText = errorMsg;
-    console.error(errorMsg);
-    return;
-  }
-};
-conformanceGet.send();
+  };
+  conformanceGet.send();
 
-function redirect(conformanceStatement) {
-  var authUri, tokenUri;
-  var smartExtension = conformanceStatement.rest[0].security.extension.filter(function(e) {
-    return e.url === "http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris";
-  });
+  function redirect(conformanceStatement) {
+    var authUri, tokenUri;
+    if (serviceUri.search('3.92.187.150') > 0) {
+      authUri = "https://3.92.187.150:8443/auth/realms/ProviderCredentials/protocol/openid-connect/auth";
+      tokenUri = "https://3.92.187.150:8443/auth/realms/ProviderCredentials/protocol/openid-connect/token"
+    } else {
+      var smartExtension = conformanceStatement.rest[0].security.extension.filter(function (e) {
+        return e.url === "http://fhir-registry.smarthealthit.org/StructureDefinition/oauth-uris";
+      });
 
-  smartExtension[0].extension.forEach(function(arg) {
-    if (arg.url === "authorize") {
-      authUri = arg.valueUri;
-    } else if (arg.url === "token") {
-      tokenUri = arg.valueUri;
+      smartExtension[0].extension.forEach(function (arg) {
+        if (arg.url === "authorize") {
+          authUri = arg.valueUri;
+        } else if (arg.url === "token") {
+          tokenUri = arg.valueUri;
+        }
+      });
     }
-  });
-  console.log(authUri);
-  console.log(tokenUri);
-  // retain a couple parameters in the session for later use
-  sessionStorage[state] = JSON.stringify({
-    clientId: clientId,
-    secret: secret,
-    serviceUri: serviceUri,
-    redirectUri: redirectUri,
-    patientId: patientId,
-    tokenUri: tokenUri
-  });
+    console.log(authUri);
+    console.log(tokenUri);
+    // retain a couple parameters in the session for later use
+    sessionStorage[state] = JSON.stringify({
+      clientId: clientId,
+      secret: secret,
+      serviceUri: serviceUri,
+      redirectUri: redirectUri,
+      patientId: patientId,
+      tokenUri: tokenUri,
+      launchContextId: launchContextId
+    });
 
-  // finally, redirect the browser to the authorizatin server and pass the needed
-  // parameters for the authorization request in the URL
-  window.location.href =
-    authUri +
-    "?" +
-    "response_type=code&" +
-    "client_id=" +
-    encodeURIComponent(clientId) +
-    "&" +
-    "scope=" +
-    encodeURIComponent(scope) +
-    "&" +
-    "redirect_uri=" +
-    encodeURIComponent(redirectUri) +
-    "&" +
-    "aud=" +
-    encodeURIComponent(serviceUri) +
-    "&" +
-    "launch=" +
-    launchContextId +
-    "&" +
-    "state=" +
-    state;
+    // finally, redirect the browser to the authorizatin server and pass the needed
+    // parameters for the authorization request in the URL
+    window.location.href =
+      authUri +
+      "?" +
+      "response_type=code&" +
+      "client_id=" +
+      encodeURIComponent(clientId) +
+      "&" +
+      "scope=" +
+      encodeURIComponent(scope) +
+      "&" +
+      "redirect_uri=" +
+      encodeURIComponent(redirectUri) +
+      "&" +
+      "aud=" +
+      encodeURIComponent(serviceUri) +
+      "&" +
+      "launch=" +
+      launchContextId +
+      "&" +
+      "state=" +
+      state;
+  }
 }
