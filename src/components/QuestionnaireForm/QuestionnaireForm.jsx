9ee2ae4fb6ddf9eb9d1e5b5a89476fad33d26d78
Jason Walonoski <jwalonoski@mitre.org>
2019-05-01 14:56:33 -0400

    Fix hard-coded URL.

--- src/components/QuestionnaireForm/QuestionnaireForm.jsx
+++ src/components/QuestionnaireForm/QuestionnaireForm.jsx
@@ -506,7 +506,7 @@ export default class QuestionnaireForm extends Component {
         priorAuthBundle.entry.unshift({ resource: priorAuthClaim })
 
         const Http = new XMLHttpRequest();
-        const priorAuthUrl = "https://davinci-prior-auth.logicahealth.org/fhir";
+        const priorAuthUrl = "https://davinci-prior-auth.logicahealth.org/fhir/Claim/$submit";
         // const priorAuthUrl = "http://localhost:9000/fhir/Claim/$submit";
         Http.open("POST", priorAuthUrl);
         Http.setRequestHeader("Content-Type", "application/fhir+json");
