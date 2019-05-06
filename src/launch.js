0e2e1137800c34aa31c0ded94dd212430d99deab
Keeyan Ghoreshi <kghoreshi@mitre.org>
2019-05-06 14:11:31 -0400

    fix bind

--- src/launch.js
+++ src/launch.js
@@ -1,6 +1,30 @@
 import urlUtils from "./util/url";
 import fetch from "node-fetch";
 
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function(oThis) {
+      if (typeof this !== 'function') {
+        // closest thing possible to the ECMAScript 5
+        // internal IsCallable function
+        throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+      }
+  
+      var aArgs   = Array.prototype.slice.call(arguments, 1),
+          fToBind = this,
+          fNOP    = function() {},
+          fBound  = function() {
+            return fToBind.apply(this instanceof fNOP && oThis
+                   ? this
+                   : oThis,
+                   aArgs.concat(Array.prototype.slice.call(arguments)));
+          };
+  
+      fNOP.prototype = this.prototype;
+      fBound.prototype = new fNOP();
+  
+      return fBound;
+    };
+  }
 // Change this to the ID of the client that you registered with the SMART on FHIR authorization server.
 var clientId = "7c47a01b-b7d8-41cf-a290-8ed607108e70";
 
