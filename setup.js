20b640fe8c964bfe56890c4f4f6d6c97955002c2
kghoreshi <kghoreshi@mitre.org>
2020-02-13 18:17:10 -0500

    debugging with hardcoded certs - bad practice i know

--- setup.js
+++ setup.js
@@ -7,9 +7,6 @@ var fs = require("fs"),
   server = jsonServer.create(),
   router = jsonServer.router("./src/db.json"),
   middlewares = jsonServer.defaults();
-  pem.config({
-    pathOpenSSL: "/usr/local/bin/openssl"
-  });
 
   server.use("/fetchFhirUri", function(req, res) {
     var url = "https://davinci-crd.logicahealth.org/fetchFhirUri" + req.url;
@@ -26,42 +23,76 @@ var fs = require("fs"),
       }})).pipe(res);
   });
 
-pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
-    if (err) {
-      throw err;
-    }
+// pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+//     if (err) {
+//       throw err;
+//     }
 
-    var options = {
-        key: keys.serviceKey,
-        cert: keys.certificate
-      };
+//     var options = {
+//         key: keys.serviceKey,
+//         cert: keys.certificate
+//       };
       
-    server.use(middlewares);
-    server.use(jsonServer.bodyParser);
+//     server.use(middlewares);
+//     server.use(jsonServer.bodyParser);
+// server.use((req, res, next) => {
+//   if (req.method === "POST") {
+//     req.body.createdAt = Date.now();
+//   }
+//   // Continue to JSON Server router
+//   next();
+// });
+
+//     server.get("/register", function(req, res){
+//         res.sendFile(__dirname + "/public/register.html");
+//     });
+//     server.get("/index", function(req, res){
+//         res.sendFile(__dirname + "/public/index.html");
+//     });
+//     server.get("/launch", function(req, res){
+//         res.sendFile(__dirname + "/public/launch.html");
+//     });
+//     server.get("/priorauth", function(req, res){
+//         res.sendFile(__dirname + "/public/priorauth.html");
+//     });
+
+//     server.use(router);
+//     https.createServer(options, server).listen(3005, "0.0.0.0", function() {
+//         console.log("json-server started on port " + 3005);
+//     });
+      
+//   });
+
+  var options = {
+    key: fs.readFileSync("./certs/server.key"),
+    cert: fs.readFileSync("./certs/server.cert")
+  };
+  
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-  if (req.method === "POST") {
-    req.body.createdAt = Date.now();
-  }
-  // Continue to JSON Server router
-  next();
+if (req.method === "POST") {
+req.body.createdAt = Date.now();
+}
+// Continue to JSON Server router
+next();
 });
 
-    server.get("/register", function(req, res){
-        res.sendFile(__dirname + "/public/register.html");
-    });
-    server.get("/index", function(req, res){
-        res.sendFile(__dirname + "/public/index.html");
-    });
-    server.get("/launch", function(req, res){
-        res.sendFile(__dirname + "/public/launch.html");
-    });
-    server.get("/priorauth", function(req, res){
-        res.sendFile(__dirname + "/public/priorauth.html");
-    });
+server.get("/register", function(req, res){
+    res.sendFile(__dirname + "/public/register.html");
+});
+server.get("/index", function(req, res){
+    res.sendFile(__dirname + "/public/index.html");
+});
+server.get("/launch", function(req, res){
+    res.sendFile(__dirname + "/public/launch.html");
+});
+server.get("/priorauth", function(req, res){
+    res.sendFile(__dirname + "/public/priorauth.html");
+});
 
-    server.use(router);
-    https.createServer(options, server).listen(3005, "0.0.0.0", function() {
-        console.log("json-server started on port " + 3005);
-    });
-      
-  });
+server.use(router);
+https.createServer(options, server).listen(3005, "0.0.0.0", function() {
+    console.log("json-server started on port " + 3005);
+});
+  
