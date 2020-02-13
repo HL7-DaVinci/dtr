f41d57f8bfae99c714189e50386c4d7582ce56dd
kghoreshi <kghoreshi@mitre.org>
2020-02-13 16:57:02 -0500

    use express server over webpack

--- setup.js
+++ setup.js
@@ -1,5 +1,6 @@
 
 var fs = require("fs"),
+  request = require("request"),
   https = require("https"),
   jsonServer = require("json-server"),
   pem = require("pem"),
@@ -7,6 +8,21 @@ var fs = require("fs"),
   router = jsonServer.router("./src/db.json"),
   middlewares = jsonServer.defaults();
 
+
+  server.use("/fetchFhirUri", function(req, res) {
+    var url = "https://davinci-crd.logicahealth.org/fetchFhirUri" + req.url;
+    console.log(url);
+
+    req.pipe(request({url:url,  agentOptions: {
+        rejectUnauthorized: false
+      }})).pipe(res);
+  });
+  server.use("/getfile", function(req, res) {
+    var url = "https://davinci-crd.logicahealth.org" + req.url;
+    req.pipe(request({url:url,  agentOptions: {
+        rejectUnauthorized: false
+      }})).pipe(res);
+  });
 
 pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
     if (err) {
@@ -27,23 +43,23 @@ server.use((req, res, next) => {
   // Continue to JSON Server router
   next();
 });
-    // server.get('/register', function(req, res){
-    //     res.sendFile(__dirname + '/public/register.html');
-    // });
-    // server.get('/index', function(req, res){
-    //     res.sendFile(__dirname + '/public/index.html');
-    // });
-    // server.get('/launch', function(req, res){
-    //     res.sendFile(__dirname + '/public/launch.html');
-    // });
-    // server.get('/priorauth', function(req, res){
-    //     res.sendFile(__dirname + '/public/priorauth.html');
-    // });
+
+    server.get("/register", function(req, res){
+        res.sendFile(__dirname + "/public/register.html");
+    });
+    server.get("/index", function(req, res){
+        res.sendFile(__dirname + "/public/index.html");
+    });
+    server.get("/launch", function(req, res){
+        res.sendFile(__dirname + "/public/launch.html");
+    });
+    server.get("/priorauth", function(req, res){
+        res.sendFile(__dirname + "/public/priorauth.html");
+    });
 
     server.use(router);
-
-    https.createServer(options, server).listen(3006, function() {
-        console.log("json-server started on port " + 3006);
+    https.createServer(options, server).listen(3005, "0.0.0.0", function() {
+        console.log("json-server started on port " + 3005);
     });
       
   });
