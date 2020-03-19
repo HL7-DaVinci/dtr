e4efa31f013feb8b031a839017f9a3cd0111bb6a
Yunwei Wang <yunwwang@gmail.com>
2020-03-19 13:40:38 -0500

    bug fix

--- src/components/QuestionnaireForm/QuestionnaireForm.jsx
+++ src/components/QuestionnaireForm/QuestionnaireForm.jsx
@@ -286,7 +286,7 @@ export default class QuestionnaireForm extends Component {
               });
           });
 
-          if (this.state.fhirVersion === 'STU3') {
+          if (this.props.fhirVersion === 'STU3') {
             item.option = choice
           } else {
             item.answerOption = choice
