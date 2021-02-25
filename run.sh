fea960c0526f81654469034a432d796eca01f170
kghoreshi <keeyan.ghoreshi@uconn.edu>
2021-02-25 17:16:11 -0500

    consolidate build and launch steps

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-echo "npm run build"
-npm run build
 echo "npm run start$VERSION"
 npm run start$VERSION
--- src/backend/app.js
+++ src/backend/app.js
@@ -6,11 +6,20 @@ const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json({ type: ["application/json", "application/fhir+json"] }));
-let version = process.env.VERSION;
-if (version !== "Dev") {
-    app.use(express.static(`${__dirname}/../../public/`));
-}
+app.use(express.static(`${__dirname}/../../public/`));
+app.get("/register", function(req, res){
+    res.sendFile("register.html", {root: "./public"});
+});
+app.get("/index", function(req, res){
+    res.sendFile("index.html", {root: "./public"});
+});
+app.get("/launch", function(req, res){
+    res.sendFile("launch.html", {root: "./public"});
+});
+app.get("/logs", function(req, res){
+    res.sendFile("logs.html", {root: "./public"});
+});
 
 app.use("/", dbRouter);
-console.log("epic");
+console.log("starting backend");
 module.exports = app;
