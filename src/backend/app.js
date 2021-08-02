3ac38226b245cf4879f3ea5ff0442e2b8a8b264d
kghoreshi <keeyan.ghoreshi@uconn.edu>
2021-08-02 14:54:58 -0400

    use fhir/filepath from appcontext

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,20 +1,10 @@
 const express = require("express");
 const dbRouter = require("./routes/database");
 const bodyParser = require("body-parser");
-const request = require("request");
-const createProxyMiddleware = require("http-proxy-middleware");
 
-function startApp(proxyTarget) {
+function startApp() {
     const app = express();
-    app.use("/fhir", createProxyMiddleware({ target: proxyTarget, changeOrigin: false }));
-    app.use("/files", function (req, res) {
-        var url = proxyTarget + "/files" + req.url;
-        req.pipe(request({
-            url: url, agentOptions: {
-                rejectUnauthorized: false
-            }
-        })).pipe(res);
-    });
+
     app.use(express.json());
     app.use(bodyParser.json({ type: ["application/json", "application/fhir+json"] }));
     app.use(express.static(`${__dirname}/../../public/`));
