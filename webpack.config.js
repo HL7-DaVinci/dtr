50f151f1b8a6bd8479f7e34b6b1aad6ca930753d
Keeyan <keeyan.ghoreshi@uconn.edu>
2021-08-05 14:19:07 -0400

    Docker test (#157)
    * console logs to see dir
    
    * try cli progress bar
    
    * revert all but one change
    
    * add progress back
    
    * check npm-run-all as source of problem

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "public/js"),
-        publicPath: "/"
+        publicPath: "./"
       },
 	watch: true,
     devtool: "#eval-source-map",
